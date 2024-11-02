ec98901c8579d7fa89c1bc2abe98420b1ec402af
Khanh Han <phamkhanhhan6@gmail.com>
2024-11-02 18:24:11 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,13 @@ module.exports = {
     },
     extend: {
       colors: {
+        grayBg: "#f5f5f5",
         blueTaskbar: "#396CF0",
         redNote: "#EF6E6E",
         greenNote: "#53C797",
         yellowNote: "#F1B561",
+        grayBut: "#D9D9D9",
+        lightBlue: "#D6E2F2",
       },
       width: {
         1296: "1296px",
