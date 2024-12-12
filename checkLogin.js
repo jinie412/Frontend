b0eacc081447272adc566ca9dfafd6c31b3cd6b0
luongtrz <luongtran1832004@gmail.com>
2024-12-12 16:43:45 +0700

    test deploy githubpage x2

--- checkLogin.js
+++ checkLogin.js
@@ -2,20 +2,15 @@ const router = () => {
     const accessToken = localStorage.getItem("accessToken");
     const currentPath = window.location.pathname;
     
-    // Lấy basePath từ cấu trúc đường dẫn hiện tại
-    const basePath = window.location.pathname.split('/')[1] === 'Frontent' ? 'Frontent' : ''; // Xử lý theo cấu trúc thư mục
-  
-    // Đảm bảo đường dẫn login chính xác
+    const basePath = window.location.pathname.split('/')[1] === 'Frontend' ? 'Frontend' : '';
+
     const loginPath = basePath ? `/${basePath}/login/index.html` : '/login/index.html';
     
-    // Nếu không có accessToken và không phải trang login, điều hướng đến trang login
     if (!accessToken && currentPath !== loginPath) {
       window.location.assign(loginPath);
     }
-    // Nếu có accessToken và đang ở trang login, xóa token và điều hướng về trang chính
     else if (accessToken && currentPath === loginPath) {
-      localStorage.removeItem("accessToken");  // Xóa token
-      window.location.assign(`/${basePath}/`);  // Điều hướng về trang chính
+      localStorage.removeItem("accessToken");
     }
   };
   
