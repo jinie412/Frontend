a895620bf900280c64f8952d2124c8d85663a532
luongtrz <luongtran1832004@gmail.com>
2024-12-12 16:50:59 +0700

    test deploy githubpage x3

--- checkLogin.js
+++ checkLogin.js
@@ -2,17 +2,16 @@ const router = () => {
     const accessToken = localStorage.getItem("accessToken");
     const currentPath = window.location.pathname;
     
-    const basePath = window.location.pathname.split('/')[1] === 'Frontend' ? 'Frontend' : '';
+    const basePath = currentPath.startsWith('/Frontend') ? 'Frontend' : '';
 
     const loginPath = basePath ? `/${basePath}/login/index.html` : '/login/index.html';
     
     if (!accessToken && currentPath !== loginPath) {
-      window.location.assign(loginPath);
+        window.location.assign(loginPath);
     }
     else if (accessToken && currentPath === loginPath) {
-      localStorage.removeItem("accessToken");
+        localStorage.removeItem("accessToken");
     }
-  };
-  
-  document.addEventListener("DOMContentLoaded", router);
-  
+};
+
+document.addEventListener("DOMContentLoaded", router);
