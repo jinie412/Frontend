80806991a9944e03fb63db62e1359bbc9a781789
luongtrz <luongtran1832004@gmail.com>
2024-12-12 16:07:34 +0700

    fix bug routering x2

--- checkLogin.js
+++ checkLogin.js
@@ -2,10 +2,10 @@ const router = () => {
   const accessToken = localStorage.getItem("accessToken");
   const currentPath = window.location.pathname;
 
-  if (!accessToken && currentPath !== "/login/index.html") {
+  if (!accessToken && currentPath !== "../login/index.html") {
     localStorage.removeItem("accessToken");
     window.location.assign("../login/index.html");
-  } else if (accessToken && currentPath === "/login/index.html") {
+  } else if (accessToken && currentPath === "../login/index.html") {
     localStorage.removeItem("accessToken");
     window.location.assign("../login/index.html");
   }
